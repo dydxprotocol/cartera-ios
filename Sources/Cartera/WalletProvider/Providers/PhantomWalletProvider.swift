9ea55d5eb9276740ec4a1fa7deb6eb6c819cb488
Rui <rui@dydx.exchange>
2025-03-05 13:07:35 -0800

    Phantom Wallet 1/n

--- Sources/Cartera/WalletProvider/Providers/PhantomWalletProvider.swift
+++ Sources/Cartera/WalletProvider/Providers/PhantomWalletProvider.swift
@@ -14,6 +14,22 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
     
     private enum CallbackAction: String {
         case onConnect
+        case onDisconnect
+        case onSignMessage
+        case onSignTransaction
+        
+        var request: String {
+            switch self {
+            case .onConnect:
+                return "connect"
+            case .onDisconnect:
+                return "disconnect"
+            case .onSignMessage:
+                return "signMessage"
+            case .onSignTransaction:
+                return "signTransaction"
+            }
+        }
     }
     
     private let baseUrlString = "https://phantom.app/ul/v1"
@@ -31,6 +47,8 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
     
     private var publicKey: Data?  = nil
     private var privateKey: Data? = nil
+    private var phantomPublicKey: Data? = nil
+    private var session: String? = nil
     
     private var publickKeyEncoded: String? {
         guard let bytes = publicKey?.bytes else {
@@ -64,6 +82,12 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
     private var foregroundToken: NotificationToken?
     private var backgroundToken: NotificationToken?
 
+    private var _walletStatus = WalletStatusImp() {
+        didSet {
+            walletStatusDelegate?.statusChanged(_walletStatus)
+        }
+    }
+    
     private var background: Bool = false {
         didSet {
             if background == false {
@@ -75,8 +99,9 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
         }
     }
     
-    private var connectionCompletions: [URL: WalletConnectCompletion] = [:]
-    
+    private var connectionCompletion:  WalletConnectCompletion?
+    private var connectionWallet: Wallet?
+    private var operationCompletion: WalletOperationCompletion?
     
     // MARK: WalletDeeplinkHandlingProtocol
       
@@ -92,27 +117,93 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
             return false
         }
         
-        let params = urlComponents.queryItems
-        
         switch action {
         case .onConnect:
-//            if params.fir
-//            if let errorCode = params["errorCode"] {
-//                let errorMessage = params["errorMessage"] ?: "Unkbown error"
-//            }
+            guard let completion = connectionCompletion else {
+                return false
+            }
+            connectionCompletion = nil
+            
+            let errorCode = url.queryParams["errorCode"]
+            let errorMessage = url.queryParams["errorMessage"] ?? "Unkbown error"
+            if let errorCode = errorCode {
+                let code = Int(errorCode) ?? -1
+                completion(nil, WalletError.error(code: .unexpectedResponse, message: errorMessage + " (\(code))"))
+            } else {
+                phantomPublicKey = base58Decode(data: url.queryParams["phantom_encryption_public_key"])
+                let nonce = url.queryParams["nonce"]
+                if let data = decryptPayload(payload: url.queryParams["data"], nonce: nonce) {
+                    if let response = try? JSONDecoder().decode(ConnectResponse.self,  from: data) {
+                        session = response.session
+                        
+                        _walletStatus.connectedWallet = WalletInfo(address: "Phantom", chainId: nil, wallet: connectionWallet)
+                        _walletStatus.state = .connectedToWallet
+                        completion(_walletStatus.connectedWallet, nil)
+//
+//                        if let sessionData = decodeSignedPayload(payload: response.session),
+//                           let sessionStruct = try? JSONDecoder().decode(PhantomSession.self, from: sessionData) {
+//                            completion(WalletInfo(address: sessionStruct.chain, chainId: nil, wallet: nil), nil)
+//                        } else {
+//                            completion(nil, WalletError.error(code: .unexpectedResponse, message: "Unable to decode session"))
+//                        }
+                    } else {
+                        completion(nil, WalletError.error(code: .unexpectedResponse, message: "Unexpected JSON payload"))
+                    }
+                } else {
+                    completion(nil, WalletError.error(code: .unexpectedResponse, message: "Unable to decrypt payload"))
+                }
+            }
             return true
             
+        case .onDisconnect:
+            let errorCode = url.queryParams["errorCode"]
+            let errorMessage = url.queryParams["errorMessage"] ?? "Unkbown error"
+            if let errorCode = errorCode {
+                Console.shared.log("PhantomWalletProvider Disconnected: \(errorMessage) (\(errorCode))")
+            }
+            return true
+            
+        case .onSignMessage:
+            guard let completion = operationCompletion else {
+                return false
+            }
+            operationCompletion = nil
+            
+            let errorCode = url.queryParams["errorCode"]
+            let errorMessage = url.queryParams["errorMessage"] ?? "Unkbown error"
+            if let errorCode = errorCode {
+                let code = Int(errorCode) ?? -1
+                completion(nil, WalletError.error(code: .unexpectedResponse, message: errorMessage + " (\(code))"))
+            } else {
+                let nonce = url.queryParams["nonce"]
+                if let data = decryptPayload(payload: url.queryParams["data"], nonce: nonce) {
+                    if let response = try? JSONDecoder().decode(SignMessageResponse.self,  from: data) {
+                        completion(response.signature, nil)
+                    } else {
+                        completion(nil, WalletError.error(code: .unexpectedResponse, message: "Unexpected JSON payload"))
+                    }
+                } else {
+                    completion(nil, WalletError.error(code: .unexpectedResponse, message: "Unable to decrypt payload"))
+                }
+            }
+            return true
+            
+        case .onSignTransaction:
+            
+            return true
         }
         
         return false
     }
-    
+
     // MARK: WalletOperationProviderProtocol
 
-    var walletStatus: (any WalletStatusProtocol)?
+    var walletStatus: WalletStatusProtocol? {
+        _walletStatus
+    }
     
-    var walletStatusDelegate: (any WalletStatusDelegate)?
-    
+    var walletStatusDelegate: WalletStatusDelegate?
+
     // MARK: WalletUserConsentOperationProtocol
 
     var userConsentDelegate: (any WalletUserConsentProtocol)?
@@ -120,6 +211,11 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
     // MARK: WalletOperationProtocol
 
     func connect(request: WalletRequest, completion: @escaping WalletConnectCompletion) {
+        guard session == nil else {
+            completion(_walletStatus.connectedWallet, nil)
+            return
+        }
+        
         guard let appUrl =  Self.appUrl, let appRedirectBaseUrl = Self.appRedirectBaseUrl else {
             assertionFailure("PhantomWalletConfig missing")
             return
@@ -137,7 +233,7 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
             return
         }
         
-        guard var urlComponents = URLComponents(string:  baseUrlString + "/connect") else {
+        guard var urlComponents = URLComponents(string:  baseUrlString + "/" + CallbackAction.onConnect.request) else {
             completion(nil, WalletError.error(code: .unexpectedResponse, message: "Failed to create URL"))
             return
         }
@@ -169,23 +265,92 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
             }
             
             // let deeplink callback handle the result
-            self?.connectionCompletions[url] = completion
+            self?.connectionCompletion = completion
+            self?.connectionWallet = request.wallet
         }
     }
     
     func disconnect() {
+        if let request = try? DisconnectRequest(session: session).json(),
+           let url = createRequestUrl(request: request, action: .onDisconnect)  {
+            openLaunchDeeplink(url: url) { success in
+                if !success {
+                    assertionFailure("Failed to open URL")
+                }
+            }
+        }
+        
         publicKey = nil
         privateKey = nil
+        phantomPublicKey = nil
         openUrlCompletions = []
-        connectionCompletions = [:]
+        connectionCompletion = nil
+        operationCompletion = nil
+        
+        self.session = nil
+        connectionWallet = nil
+        _walletStatus.state = .idle
+        _walletStatus.connectedWallet = nil
     }
     
     func signMessage(request: WalletRequest, message: String, connected: WalletConnectedCompletion?, completion: @escaping WalletOperationCompletion) {
-        
+        connect(request: request) { [weak self] walletInfo, error in
+            if let error = error {
+                LocalAuthenticator.shared?.paused = false
+                completion(nil, error)
+            } else {
+                connected?(walletInfo)
+                self?.doSignMessage(message: message, completion: completion)
+            }
+        }
+    }
+    
+    private func doSignMessage(message: String,  completion: @escaping WalletOperationCompletion) {
+        let request = SignMessageRequest(session: session,
+                                         message: base58Encode(data: message.data(using: .utf8)),
+                                         display: "utf8")
+        if let request = try? request.json(),
+           let url = createRequestUrl(request: request, action: .onSignMessage) {
+            openLaunchDeeplink(url: url) { [weak self] success in
+                if !success {
+                    assertionFailure("Failed to open URL")
+                }
+                
+                self?.operationCompletion = completion
+            }
+        }
     }
     
     func sign(request: WalletRequest, typedDataProvider: (any WalletTypedDataProviderProtocol)?, connected: WalletConnectedCompletion?, completion: @escaping WalletOperationCompletion) {
+        connect(request: request) { [weak self] walletInfo, error in
+            if let error = error {
+                LocalAuthenticator.shared?.paused = false
+                completion(nil, error)
+            } else {
+                connected?(walletInfo)
+                self?.doSign(typedDataProvider: typedDataProvider, completion: completion)
+            }
+        }
+    }
+    
+    private func doSign(typedDataProvider: (any WalletTypedDataProviderProtocol)?, completion: @escaping WalletOperationCompletion) {
+        guard let typeDataString = typedDataProvider?.typedDataAsString else {
+            completion(nil, WalletError.error(code: .invalidInput, message: "invalid typedData"))
+            return
+        }
         
+        let request = SignTransactionRequest(session: session,
+                                             transaction: base58Encode(data: typeDataString.data(using: .utf8)))
+        if  let request = try? request.json(),
+            let url = createRequestUrl(request: request, action: .onSignTransaction) {
+            openLaunchDeeplink(url: url) { [weak self] success in
+                if !success {
+                    assertionFailure("Failed to open URL")
+                }
+                
+                self?.operationCompletion = completion
+            }
+        }
     }
     
     func send(request: WalletTransactionRequest, connected: WalletConnectedCompletion?, completion: @escaping WalletOperationCompletion) {
@@ -193,7 +358,87 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
     }
     
     func addChain(request: WalletRequest, chain: EthereumAddChainRequest, timeOut: TimeInterval?, connected: WalletConnectedCompletion?, completion: @escaping WalletOperationCompletion) {
+        // no-op
+    }
+    
+    private func createRequestUrl(request: String?, action: CallbackAction) -> URL? {
+        guard var urlComponents = URLComponents(string:  baseUrlString + "/" + action.request),
+              let appRedirectBaseUrl = Self.appRedirectBaseUrl else {
+            return nil
+        }
         
+        if  let request = request,
+            let result = encryptPayload(payload: request.data(using: .utf8)),
+            let payload = base58Encode(data: result.0),
+            let nonce = base58Encode(data: result.1) {
+            var queryItems = [URLQueryItem]()
+            queryItems.append(URLQueryItem(name: "payload", value: payload))
+            queryItems.append(URLQueryItem(name: "nonce", value: nonce))
+            queryItems.append(URLQueryItem(name: "redirect_link", value: appRedirectBaseUrl + "/" + action.rawValue))
+            queryItems.append(URLQueryItem(name: "dapp_encryption_public_key", value: publickKeyEncoded))
+            
+            urlComponents.queryItems = queryItems
+            
+            return urlComponents.url
+        }
+        
+        return nil
+       
+    }
+    
+    private func base58Encode(data: Data?) -> String? {
+        if let data = data {
+            return Base58.base58Encode(data.bytes)
+        }
+        return nil
+    }
+    
+    private func base58Decode(data: String?) -> Data? {
+        if let data = data, let bytes = Base58.base58Decode(data) {
+            return Data(bytes: bytes, count: bytes.count)
+        }
+        return nil
+    }
+    
+    private func encryptPayload(payload: Data?) -> (Data, Data)? {
+         let nonceData = Data.randomBytes(count: 24)
+        
+        if let payload = payload,
+           let publicKey = phantomPublicKey,
+           let privateKey = privateKey {
+            let encryptedData = try? NaclBox.box(message: payload,
+                                                 nonce: nonceData,
+                                                 publicKey: publicKey,
+                                                 secretKey: privateKey)
+            if let encryptedData = encryptedData  {
+                return (encryptedData, nonceData)
+            }
+            return nil
+        }
+        return nil
+    }
+    
+    private func decryptPayload(payload: String?, nonce: String?) -> Data? {
+        if let decodedData = base58Decode(data: payload),
+           let decodedNonceData = base58Decode(data: nonce),
+           let publicKey = phantomPublicKey,
+           let privateKey = privateKey {
+            return try? NaclBox.open(message: decodedData,
+                                     nonce:  decodedNonceData,
+                                     publicKey: publicKey,
+                                     secretKey: privateKey)
+        }
+        
+        return nil
+    }
+    
+    private func decodeSignedPayload(payload: String?) -> Data? {
+        if let decodedData = base58Decode(data: payload),
+           let publicKey = phantomPublicKey {
+            return try? NaclSign.signOpen(signedMessage: decodedData, publicKey: publicKey)
+        }
+        
+        return nil
     }
     
     private func openLaunchDeeplink(url: URL, completion: ((Bool) -> Void)? = nil) {
@@ -209,5 +454,63 @@ final class PhantomWalletProvider: NSObject, WalletOperationProviderProtocol {
              openUrlCompletions.append((url, completion))
         }
     }
-    
+}
+
+private extension Data {
+    /// Returns cryptographically secure random data.
+    ///
+    /// - Parameter length: Length of the data in bytes.
+    /// - Returns: Generated data of the specified length.
+    static func random(length: Int) throws -> Data {
+        return Data((0 ..< length).map { _ in UInt8.random(in: UInt8.min ... UInt8.max) })
+    }
+}
+
+private extension URL {
+    var queryParams: [String: String] {
+        guard let urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: true) else {
+            return [:]
+        }
+       
+        var queryParams: [String: String] = [:]
+        for query in urlComponents.queryItems ?? [] {
+            queryParams[query.name] = query.value
+        }
+        return queryParams
+    }
+}
+
+private struct ConnectResponse: Codable {
+    let public_key: String?
+    let session: String?
+}
+
+private struct DisconnectRequest: Codable {
+    let session: String?
+}
+
+private struct SignMessageRequest: Codable {
+    let session: String?
+    let message: String?
+    let display: String?    //  "utf8" | "hex"
+}
+
+private struct SignMessageResponse: Codable {
+    let signature: String?
+}
+
+private struct SignTransactionRequest: Codable {
+    let session: String?
+    let transaction: String?
+}
+
+private struct SignTransactionResponse: Codable {
+    let transaction: String?
+}
+
+private struct PhantomSession: Codable {
+    let app_url: String?
+    let timestamp: String?
+    let chain: String?
+    let cluster: String?
 }
